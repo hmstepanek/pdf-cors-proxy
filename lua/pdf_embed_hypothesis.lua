ca0d7d30bfcd2c12fec4bd27471dd46c9033b36b
Hannah Stepanek <hmstepanek@hypothes.is>
2019-09-25 15:57:04 -0700

    Make /pdf/ endpoint internal

--- lua/pdf_embed_hypothesis.lua
+++ lua/pdf_embed_hypothesis.lua
@@ -2,7 +2,7 @@ local template = require("resty.template")
 local template_string = ngx.location.capture("/templates/pdfjs_viewer.html")
 local query_params = ngx.req.get_query_args()
 template.render(template_string.body, {
-    url = ngx.var.request_uri:sub(6),
+    url = ngx.var.request_uri:sub(2),
     h_embed_url = os.getenv("H_EMBED_URL"),
     h_request_config = query_params["via.request_config_from_frame"],
     h_open_sidebar = query_params["via.open_sidebar"],
