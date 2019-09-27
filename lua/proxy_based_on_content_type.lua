244675f37d08b6209fdf917f33364f9d4ac196dc
Hannah Stepanek <hmstepanek@hypothes.is>
2019-09-27 11:10:55 -0700

    Issue HEAD request, fallback to GET request

--- lua/proxy_based_on_content_type.lua
+++ lua/proxy_based_on_content_type.lua
@@ -1,4 +1,9 @@
-local response = ngx.location.capture("/id_" .. ngx.var.request_uri)
+local response = ngx.location.capture(ngx.var.request_uri, { method = ngx.HTTP_HEAD})
+if (response.header["Content-Type"] == nil) then
+    ngx.log(ngx.STDERR, "HEAD request for " .. ngx.var.request_uri .. " didn't have Content-Type header.")
+    response = ngx.location.capture("/id_" .. ngx.var.request_uri)
+end
+
 if (response.header["Content-Type"] == "application/pdf" 
         or response.header["Content-Type"] == "application/x-pdf") then
     return ngx.exec("/pdf" .. ngx.var.request_uri)
